5f1a1b955390bc9547b0bdc4eb5be3bae5b2805f
Juan Garcia <juagarca@gmail.com>
2021-01-11 13:30:44 +0000

    created seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Message.destroy_all
+User.destroy_all
+Channel.destroy_all
+
+general = Channel.create(name: "General")
+ruby = Channel.create(name: "Ruby")
+javascript = Channel.create(name: "JavaScrip")
+python = Channel.create(name: "Python")
+
+test_user = User.create(email:"test@test.com", password: "123456")
+juan_user = User.create(email:"juan@test.com", password: "123456")
+
+Message.create(user: test_user, channel: general, content: "first message")
+Message.create(user: test_user, channel: general, content: "second message")
+Message.create(user: juan_user, channel: ruby, content: "first message")
+Message.create(user: test_user, channel: general, content: "second message")
+Message.create(user: test_user, channel: javascript, content: "first message")
+Message.create(user: juan_user, channel: python, content: "first message")
+Message.create(user: test_user, channel: general, content: "second message")
